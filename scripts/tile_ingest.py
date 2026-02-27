d3ec498f3fa231fcb8c695c9f4644a5ac8e1640b
jkf735 <fennesseyjack@gmail.com>
2026-02-26 20:54:09 -0600

    added functions for tile ingest

---
+++ scripts/tile_ingest.py
@@ -0,0 +1,245 @@
+import os
+import json
+import logging
+import argparse
+import itertools
+import subprocess
+import geopandas as gpd
+from sqlalchemy import create_engine, text
+from shapely.ops import unary_union
+
+from config import RESOURCE_DIR, SENTINEL_PATH, RAW_BANDS_DIR, DB_URI
+
+logger = logging.getLogger("tile_ingets")
+
+class AWS_INTERFACE:
+    def __init__(self):
+        try:
+            with open(os.path.join(RESOURCE_DIR, "tile_info_cache.json"), 'r', encoding='utf-8') as f:
+                self.tile_day_cache = json.load(f)
+            self.tile_case_from_file = True
+            logger.info("Successfully loadded tile_day_cache from resources")
+        except:
+            self.tile_day_cache = {}
+            self.tile_case_from_file = False
+            logger.warning("Failed to load tile_day_cache from resources")
+        self.aws_bucket ="s3://sentinel-s2-l2a/tiles"
+        self.aws_args = ["--no-sign-request"]
+
+    def break_down_tile(self,tile)->tuple:
+        """Break down a tile string into Zone, lat and grid"""
+        try:
+            zone = int(tile[0:2])
+            tile = tile[2:]
+        except:
+            zone = int(tile[0])
+            tile = tile[1:]
+        grid_square = tile[-2:]
+        lat_band = tile[:-2]
+        return zone, lat_band, grid_square
+    
+    def list_s3(self,prefix):
+        """Return list of objects/folders under S3 prefix"""
+        cmd = ["aws", "s3", "ls", prefix] + self.aws_args
+        result = subprocess.run(cmd, capture_output=True, text=True)
+        if result.returncode != 0:
+            logger.error(f"Error in list_s3: {result.stderr}")
+            raise RuntimeError(result.stderr)
+        lines = result.stdout.splitlines()
+        # Only keep folders (lines ending with '/')
+        folders = [l.split()[-1].replace("/", "") for l in lines if l.strip().startswith("PRE")]
+        return folders
+
+    def tile_day_passes(self, tile_path, day, max_cloud, min_coverage) -> bool:
+        """Determine if a tile for given month/day meets cloud and coverage criteria, store results"""
+        key = (tile_path, day)
+        # reuse previous result
+        if key in self.tile_day_cache:
+            return self.tile_day_cache[key]
+
+        # build the path to tileinfo.json (for simplicity just use index 0)
+        tileinfo_path = f"{tile_path}{day}/0/tileInfo.json"
+        # use aws cli to get file content
+        cmd = ["aws", "s3", "cp", tileinfo_path, "-"] + self.aws_args
+        result = subprocess.run(cmd, capture_output=True, text=True)
+        # missing file → fail
+        if result.returncode != 0:
+            self.tile_day_cache[key] = False
+            logger.info(f'File not found at {cmd}')
+            return False
+
+        info = json.loads(result.stdout)
+        cloud = info.get("cloudyPixelPercentage", 100)
+        data = info.get("dataCoveragePercentage", 0)
+        passes = (cloud <= max_cloud and data >= min_coverage)
+        self.tile_day_cache[key] = passes
+        if not passes:
+            logger.info(f'Failed Cloud or data coverage check: cloud_coverage={cloud}, data_coverage={data}')
+        return passes
+
+    def find_best_tile(self,tile_list:list, year, month, max_cloud=10, min_coverage=80) -> tuple:
+        """Find first L2A tile for given month/day meeting cloud and coverage criteria"""
+        for combo in tile_list:
+            logger.info(f'Checking tile combination {combo}...')
+            # get all the candidate days
+            candidate_days = []
+            for tile in combo:
+                zone, lat_band, grid_square = self.break_down_tile(tile)
+                prefix = f"{self.aws_bucket}/{zone}/{lat_band}/{grid_square}/{year}/{month}/"
+                days = self.list_s3(prefix)
+                if candidate_days == []: 
+                    candidate_days = days
+                else:
+                    candidate_days = list(set(candidate_days).intersection(set(days)))
+            candidate_days = sorted(candidate_days, key=int)
+            for day in candidate_days:
+                logger.info(f'Checking day {day}...')
+                all_tiles_good = True
+                for tile in combo:
+                    # if a day fails then break and move to the next day
+                    zone, lat_band, grid_square = self.break_down_tile(tile)
+                    tile_path = f"{self.aws_bucket}/{zone}/{lat_band}/{grid_square}/{year}/{month}/"
+                    if not self.tile_day_passes(tile_path, day, max_cloud, min_coverage):
+                        all_tiles_good = False
+                        break
+                if all_tiles_good:
+                    logger.info(f"Found combo {combo} on day {day}")
+                    return combo, day
+        logger.warning("ABORTING RUN IN 'find_best_tile': Could Not find any tile combination that works for given park.")
+        return None, None
+
+    def download_tile_jp2s(self, tile_data:dict, output_dir, bands=['B04','B08']):
+        try:
+            tile = tile_data['tile']
+            year = tile_data['year']
+            month = tile_data['month']
+            day = tile_data['day']
+        except KeyError as e:
+            logger.error(f"Error in download_tile_jp2s: Missing key: {e}")
+            raise KeyError(f"Missing key: {e}")
+        zone, lat_band, grid_square = self.break_down_tile(tile)
+        # assume always R10m
+        base_tile_path = f"{self.aws_bucket}/{zone}/{lat_band}/{grid_square}/{year}/{month}/{day}/0/R10m/"
+        for band in bands:
+            tile_path = base_tile_path + f"{band}.jp2"
+            file_name = f"{tile}_{year}_{month}_{day}_{band}.jp2"
+            output = os.path.join(output_dir, file_name)
+            cmd = ["aws", "s3", "cp", tile_path, output] + self.aws_args
+            result = subprocess.run(cmd, capture_output=True, text=True)
+            if result.returncode != 0:
+                logger.error(f"Error in download_tile_jp2s: {result.stderr}")
+                raise RuntimeError(result.stderr)
+            logger.info(f"Successfully downloaded tile to {output}")
+
+def check_if_needed_files_exist(required_tiles, park, year, month) -> tuple:
+    """Check if any combination of needed tiles have already been downloaded"""
+    folder_path = os.path.join(RAW_BANDS_DIR, park.lower())
+    if not os.path.isdir(folder_path):
+        return False, None
+    files = sorted(os.listdir(folder_path))
+    found_tiles = []
+    for file in files:
+        split = file.split('_')
+        file_tile = split[0]
+        file_year = split[1]
+        file_month = split[2]
+        if file_year ==  str(year) and file_month == str(month):
+            if file_tile not in found_tiles:
+                found_tiles.append(file_tile)
+    for combo in required_tiles:
+        if set(combo).issubset(found_tiles):
+            return True, combo
+    return False, None
+        
+def find_tiles(park_name:str) -> list:
+    """Return list of lists of tile combinations that cover the full park area"""
+    # load sentinel shapefile
+    tiles_gdf = gpd.read_file(SENTINEL_PATH)
+    if tiles_gdf.empty:
+        logger.error(f"Error in find_tiles: Issue opening Sentinel Shapefile at {SENTINEL_PATH}")
+        raise ValueError(f"Issue opening Sentinel Shapefile at {SENTINEL_PATH}")
+    # connect to PostGIS
+    engine = create_engine(DB_URI)
+    # pull park boundary
+    query = f"""
+        SELECT geom
+        FROM parks_validated
+        WHERE park_name ILIKE '{park_name}%'
+    """
+    park_gdf = gpd.read_postgis(text(query), engine, geom_col="geom")
+    if park_gdf.empty:
+        logger.warning(f"ABORTING RUN IN 'find_tiles': No {park_name} geometry found in database.")
+        raise ValueError(f"No {park_name} geometry found in database.")
+    park_gdf = park_gdf.to_crs(tiles_gdf.crs)
+    park_geom = park_gdf.geom.iloc[0]   
+    # get intersecting tiles
+    intersecting_tiles = tiles_gdf[tiles_gdf.intersects(park_geom)].copy()
+    # reproject those tiles and find the intersecton area in m^2
+    utm_crs = park_gdf.estimate_utm_crs()
+    park_proj = park_gdf.to_crs(utm_crs)
+    park_proj_geom = park_proj.geometry.iloc[0]
+    tiles_proj = intersecting_tiles.to_crs(utm_crs)
+    tiles_proj["intersection_geom"] = tiles_proj.geometry.intersection(park_proj_geom)
+    tiles_proj["intersection_area"] = tiles_proj["intersection_geom"].area
+    # get every combination of full coverage in order from least to most tiles (sub-order by coverage area)
+    tiles_proj = tiles_proj.sort_values(by="intersection_area")
+    geoms = tiles_proj.geometry.tolist()
+    names = tiles_proj["Name"].tolist()
+    n = len(geoms)
+    list_of_lists = []
+    for r in range(1, n+1):
+        for combo_indices in itertools.combinations(range(n), r):
+            combined = unary_union([geoms[i] for i in combo_indices])
+            if combined.contains(park_proj_geom):
+                list_of_lists.append([names[i] for i in combo_indices])
+
+    return list_of_lists
+
+def ingest_tiles(park:str, year, month):
+    park = park.capitalize()
+    required_tiles = find_tiles(park)
+    files_exist, combo = check_if_needed_files_exist(required_tiles, park, year, month)
+    if not files_exist:
+        logger.info(f'No suitable combination of files are currently downloaded for {park} on {year}-{month}')
+        logger.info("Searching AWS S3 for suitable tiles...")
+        aws_interface = AWS_INTERFACE()
+        combo, day = aws_interface.find_best_tile(required_tiles,year,month)
+        if not combo or not day:
+            return
+        output_path = os.path.join(RAW_BANDS_DIR, park.lower())
+        for tile in combo:
+            tile_data = {
+                'tile': tile,
+                'year': year,
+                'month': month,
+                'day': day
+            }
+            logger.info(f"Attempting to download bands for tile {tile}...")
+            aws_interface.download_tile_jp2s(tile_data, output_path)
+            if aws_interface.tile_case_from_file:
+                logger.info(f"Updating tile_info_cashe file with new tile info")
+                with open(os.path.join(RESOURCE_DIR, "tile_info_cache.json"), 'w') as f:
+                    json.dump(aws_interface.tile_day_cache, indent=4)
+    else:
+        logger.info(f'Required tiles ({combo}) exist for {park} on {year}-{month}')
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        handlers=[
+        logging.FileHandler("logs/tile_ingest.log"),
+        logging.StreamHandler()
+    ]
+    )
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--park", type=str, required=True)
+    parser.add_argument("--year", type=int, required=True)
+    parser.add_argument("--month", type=int, required=True)
+
+    args = parser.parse_args()
+    logging.info(f'STARTING TILE INGEST FOR {args.park}, {args.year}, {args.month}')
+    ingest_tiles(args.park, args.year, args.month)
+    logging.info(f'COMPLETED TILE INGEST FOR {args.park}, {args.year}, {args.month}')
+    
