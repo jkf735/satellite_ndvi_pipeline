7ade0df45adb1ff20ddf944ebd315911af1a64a7
jkf735 <fennesseyjack@gmail.com>
2026-02-27 23:00:31 -0600

    compute_ndvi updated to handel .tifs

--- scripts/compute_ndvi.py
+++ scripts/compute_ndvi.py
@@ -1,57 +1,168 @@
+import os
+import logging
+import argparse
 import rasterio
 import numpy as np
-import os
-#TODO NOT USING CURRENTLY BUT WILL NEED IF BANDS HAVE DIFFERENT RESOLUTION (CURRENTLY BOTH 10m)
-from rasterio.enums import Resampling
 
-from resources.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+from resources.config import PROCESSED_DATA_DIR, INTERIM_DATA_DIR
 
-def compute_ndvi(red_path:str, nir_path:str, output_path:str)->None:
+logger = logging.getLogger("compute_ndvi")
 
-    #TODO future considerations below:
+def find_files(folder, year, month) -> tuple:
     """
-    # Resolution safety
-    if red.shape != nir.shape:
-        raise ValueError("Band dimensions mismatch")
-    # Reflectance Scaling
-    red = red / 10000
-    nir = nir / 10000
-    # NoData handling
-    red = np.where(red == 0, np.nan, red)
-    nir = np.where(nir == 0, np.nan, nir)
-    # Windowed reading for larger mosaics (currently reading entire raster into memory)
-    """
+    Look for the red (B04) and nir (B08) .tif files in a given folder for a give year, month and return their paths
 
-    with rasterio.open(red_path) as red_src:
-        # Sentinel bands are single-band so always read the 1st band
+    Parameters
+    ----------
+    folder:
+        path to interim/{park} folder
+    year: int or str
+        year being looked at (e.g. 2025)
+    month: int or str
+        month being looked at (e.g. 11)
+        
+    Returns
+    ----------
+    (red_band_path, nir_band_path, output_path): tuple
+        B04_band_path, B08_band_path, path_to_new_output
+    """
+    year = str(year)
+    month = str(month)
+    if not os.path.isdir(folder):
+        logger.warning(f"Input folder not found: {folder}")
+        raise FileNotFoundError(f"Input folder not found: {folder}")
+    files = sorted(os.listdir(folder))
+    red_file = ""
+    nir_file = ""
+    for file in files:
+        split = file.split('_')
+        file_year = split[0]
+        file_month = split[1]
+        file_band = split[3]
+        if file_year ==  str(year) and file_month == str(month):
+            if file_band == "B04": red_file = file
+            elif file_band == "B08": nir_file = file
+
+    if red_file == "":
+        logger.error(f"B04 (red) file not found in: {folder} for year:{year}, month:{month}")
+        raise FileNotFoundError(f"B04 (red) file not found in: {folder} for year:{year}, month:{month}")
+    if nir_file == "":
+        logger.error(f"B08 (nir) file not found in: {folder} for year:{year}, month:{month}")
+        raise FileNotFoundError(f"B08 (nir) file not found in: {folder} for year:{year}, month:{month}")
+    output_file = red_file.replace('B04','NDVI')
+    red_path = folder / red_file
+    nir_path = folder / nir_file
+    output_path = folder / output_file
+
+    logger.info(f"Found input files: red:{red_path}, nir:{nir_path}")
+    return red_path, nir_path, output_path
+
+def compute_ndvi_from_tif(red_path: str, nir_path: str, output_path: str) -> bool:
+    """
+    Compute NDVI from band tif and write GeoTIFF.
+
+    NDVI = (NIR - RED) / (NIR + RED)
+
+    Parameters
+    ----------
+    red_path : str
+        Path to B04 .tif (Red band)
+    nir_path : str
+        Path to B08 .tif (NIR band)
+    output_path : str
+        Path where NDVI GeoTIFF will be written
+
+    Returns
+    ----------
+    bool : If NDVI was computed or not
+    """
+    if os.path.exists(output_path):
+        logger.warning(f'Required mosaic tiles already exist at {output_path}. If you want to compute NDVI again, delete the current tif and run again.')
+        return False
+
+    if not os.path.exists(red_path):
+        logger.error(f"Red band not found: {red_path}")
+        raise FileNotFoundError(f"Red band not found: {red_path}")
+
+    if not os.path.exists(nir_path):
+        logger.error(f"NIR band not found: {nir_path}")
+        raise FileNotFoundError(f"NIR band not found: {nir_path}")
+
+    with rasterio.open(red_path) as red_src, rasterio.open(nir_path) as nir_src:
+        # validate alignment
+        logger.info("Validating Inputs...")
+        if red_src.crs != nir_src.crs:
+            logger.error("CRS mismatch between red and NIR mosaics")
+            raise ValueError("CRS mismatch between red and NIR mosaics")
+
+        if red_src.transform != nir_src.transform:
+            logger.error("Transform mismatch between red and NIR mosaics")
+            raise ValueError("Transform mismatch between red and NIR mosaics")
+
+        if red_src.width != nir_src.width or red_src.height != nir_src.height:
+            logger.error("Dimension mismatch between red and NIR mosaics")
+            raise ValueError("Dimension mismatch between red and NIR mosaics")
+        
+        
+
+        # read data
+        logger.info("Reading Inputs...")
         red = red_src.read(1).astype("float32")
-        profile = red_src.profile
-    
-    with rasterio.open(nir_path) as nir_src:
         nir = nir_src.read(1).astype("float32")
-    
-    # avoid divide by zero errors
-    np.seterr(divide='ignore', invalid='ignore')
-    # calculate ndvi
-    ndvi = (nir-red)/(nir+red)
-    # convert any divide-by-0 to np.nan (undefined)
-    ndvi = np.where((nir+red)==0, np.nan, ndvi)
-    # update our profile for float32 NDVI, single band, lzw compressed
-    profile.update(
-        driver="GTiff",
-        dtype=rasterio.float32,
-        count=1,
-        compress='lzw'
-    )
+        red_nodata = red_src.nodata
+        nir_nodata = nir_src.nodata
+        mask = None
+        if red_nodata is not None and nir_nodata is not None:
+            mask = (red == red_nodata) | (nir == nir_nodata)
 
-    with rasterio.open(output_path, 'w', **profile) as dst:
+        # compute NDVI
+        logger.info("Computing NDVI...")
+        np.seterr(divide="ignore", invalid="ignore")
+        ndvi = (nir - red) / (nir + red)
+        # mask invalid pixels
+        ndvi[(nir + red) == 0] = np.nan
+        if mask is not None:
+            ndvi[mask] = np.nan
+
+        # prepare output profile
+        profile = red_src.profile.copy()
+        profile.update(
+            driver="GTiff",
+            dtype=rasterio.float32,
+            count=1,
+            compress="lzw",
+            nodata=np.nan
+        )
+
+    # write output
+    logger.info("Writting To Output...")
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with rasterio.open(output_path, "w", **profile) as dst:
         dst.write(ndvi.astype(rasterio.float32), 1)
-    
-    print(f"NDVI written to {output_path}")
 
+    logger.info(f"NDVI .tif Successfully written to: {output_path}")
+    return True
 
 if __name__ == "__main__":
-    output_path = PROCESSED_DATA_DIR / "2025-11-14_ndvi.tif"
-    red_path = RAW_DATA_DIR / "2025-11-14_B04.jp2"
-    nir_path = RAW_DATA_DIR / "2025-11-14_B08.jp2"
-    compute_ndvi(red_path, nir_path, output_path)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+        handlers=[
+        logging.FileHandler("logs/compute_ndvi.log"),
+        logging.StreamHandler()
+    ]
+    )
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--park", type=str, required=True)
+    parser.add_argument("--year", type=int, required=True)
+    parser.add_argument("--month", type=int, required=True)
+    args = parser.parse_args()
+    input_folder = INTERIM_DATA_DIR / args.park.lower()
+    
+    logging.info(f'COMPUTING NDVI FOR {args.park}, {args.year}, {args.month}')
+    red_path, nir_path, output_path = find_files(input_folder, args.year, args.month)
+    complete = compute_ndvi_from_tif(red_path, nir_path, output_path)
+    if complete:
+        logging.info(f'COMPLETED NDVI COMPUTATION {args.park}, {args.year}, {args.month}')
+    else:
+        logging.info(f'NDVI SPKIPPED: file alread exists for {args.park}, {args.year}, {args.month} at {output_path}')
