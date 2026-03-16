525009b745289d9bf00973c6ab72015baedc74ec
jkf735 <fennesseyjack@gmail.com>
2026-03-16 15:35:25 -0500

    adding titiler_service for render deployment

---
+++ titiler_service/main.py
@@ -0,0 +1,12 @@
+import os
+os.environ["AWS_NO_SIGN_REQUEST"] = "YES"
+
+from titiler.application import app
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
